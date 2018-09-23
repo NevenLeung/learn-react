c161ab5ace3bb111cceb89b76c269a38f1a6fec7
Neven Leung <nevyn.leong@gmail.com>
2018-09-23 23:29:03 +0800

    [Updated] render-props demo.

--- src/demo/22-Render-Props/index.js
+++ src/demo/22-Render-Props/index.js
@@ -6,6 +6,7 @@ function Title() {
   )
 }
 
+// The origin mouse tracker component
 class MouseTracker extends Component {
   constructor(props) {
     super(props);
@@ -24,9 +25,59 @@ class MouseTracker extends Component {
 
   render() {
     return (
-      <div style={{height: '100%'}} onMouseMove={this.handleMouseMove}>
+      <div style={{height: '100%', border: '2px solid #eee', borderRadius: '10px'}} onMouseMove={this.handleMouseMove}>
         <h1>Move the mouse around!</h1>
         <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
+        <h3>This example use wraps all thing in one component. It is hard to reuse it.</h3>
+      </div>
+    );
+  }
+}
+
+// Another example use render props
+
+function Cat(props) {
+  const mouse = props.mouse;
+
+  return (
+    <p>The current mouse position is ({mouse.x}, {mouse.y}). The kitty is gonna to catch it.</p>
+  );
+}
+
+class Mouse extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 0,
+      y: 0
+    };
+  }
+
+  handleMouseMove = (e) => {
+    this.setState({
+      x: e.clientX,
+      y: e.clientY
+    });
+  };
+
+  render() {
+    return (
+      <div style={{height: '100%', border: '2px solid #eee', borderRadius: '10px'}} onMouseMove={this.handleMouseMove}>
+        {this.props.render(this.state)}
+      </div>
+    );
+  }
+}
+
+class NewMouseTracker extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Move the mouse around!</h1>
+        <Mouse render={mouse => (
+          <Cat mouse={mouse}/>
+        )}/>
+        <h3>This example use 'render props' technique, which is good at reusability.</h3>
       </div>
     );
   }
@@ -38,6 +89,7 @@ function Demo() {
       <Title/>
       <hr/>
       <MouseTracker/>
+      <NewMouseTracker/>
     </div>
   )
 }
